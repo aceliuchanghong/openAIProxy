a9d98891cc61020af08723b7cc5cfe6f2cb693ad
Lawrence-AIP <liuch@allinfinance.com>
2023-07-11 19:33:21 +0800

    openAi模型名称输出

--- proxyUsage/listModel.py
+++ proxyUsage/listModel.py
@@ -1,5 +1,6 @@
 import os
 import openai
+import json
 
 proxyHost = "127.0.0.1"
 proxyPort = 10809
@@ -11,4 +12,7 @@ proxies = {
 openai.proxy = proxies
 openai.api_key = os.getenv("OPENAI_API_KEY")
 print(openai.api_key)
-print(openai.Model.list())
+data = openai.Model.list()
+model_ids = [item['id'] for item in data['data']]
+
+print(model_ids)

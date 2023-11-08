cf246cbf6aa8826880ac2daa5a89c3599a2ebe9f
Lawrence-AIP <liuch@allinfinance.com>
2023-11-08 08:47:55 +0800

    suc

--- getResponse/reponse.py
+++ getResponse/reponse.py
@@ -13,9 +13,9 @@ proxies = {
 openai.proxy = proxies
 
 completion = openai.ChatCompletion.create(
-    model="gpt-4-0613",  # gpt-3.5-turbo
+    model="gpt-4-1106-preview",  # gpt-3.5-turbo gpt-4-1106-preview gpt-4-0613
     messages=[
-        {"role": "system", "content": "尝试一下写一首七言绝句诗,每行的结尾分别是:丽,看,完,离"},
+        {"role": "system", "content": "项目名称 GDSN ,统一监管报送有关 其中GDSN是汉字拼音首字母缩写,猜GDSN的汉字"},
         #{"role": "system", "content": "免费使用chatgpt4的方法"}
     ]
 )

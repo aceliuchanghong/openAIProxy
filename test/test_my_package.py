8808b0c6db10ebb48286f8f3818c70a7fc3fb409
Lawrence-AIP <liuch@allinfinance.com>
2024-01-05 16:55:39 +0800

    push

---
+++ test/test_my_package.py
@@ -0,0 +1,22 @@
+from mp4_utils.converters.mp4_to_gif import MP4ToGIFConverter
+from mp4_utils.converters.mp4_to_mp3 import MP4ToMP3Converter
+from mp4_utils.converters.mp4_to_pngs import MP4ToPNGsConverter
+from mp4_utils.converters.tv_to_mp4 import TVToMP4Converter
+from mp4_utils.core.mp4_handler import MP4Handler
+
+
+def main(converter_type, file_path='../../testfiles/onboard_cover.mp4') -> MP4Handler:
+    """
+    Convert MP4 each frame.
+    :param converter_type:
+    :param file_path:
+    """
+    Converter = {
+        "gif": MP4ToGIFConverter,
+        "mp3": MP4ToMP3Converter,
+        "img": MP4ToPNGsConverter,
+        "other": TVToMP4Converter,
+    }
+    # converter = Converter[converter_type](file_path)
+    converter = Converter.get(converter_type)(file_path)
+    return converter
